7db3a158b1b987b53392588c11b64aea80b0da46
David Torrijos <david@redradix.com>
2020-06-22 11:06:44 +0200

    Create pug components structure, browserSync and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
-const { parallel, src, dest } = require('gulp')
+const { dest, parallel, series, src, watch } = require('gulp')
+const browserSync = require('browser-sync').create()
 const pug = require('gulp-pug')
 
-const pugCompile = cb => {
-  src('src/views/*.pug')
+const pugCompile = () => (
+  src('./src/views/*.pug')
     .pipe(pug({ pretty: true }))
     .pipe(dest('./build'))
-  return cb()
+    .pipe(browserSync.stream())
+)
+
+const server = () => (
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    }
+  })
+)
+
+const watcher = () => {
+  watch('./src/**/*.pug', series(pugCompile))
 }
 
-exports.default = parallel(pugCompile)
+exports.default = series(pugCompile, parallel(server, watcher))
